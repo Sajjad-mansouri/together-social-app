608f704d76af599011dc40cfca4ca8fe0a7597d5
Sajjad Mansouri <s.mansouri4w@gmail.com>
2024-03-19 12:45:36 +0330

    add link to edit profile,logout and change password

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 from . import emailconf
 
 urlpatterns=[
-	path('profile',views.UpdateProfile.as_view(),name='update_profile'),
+	
 	path('edit-profile/',views.UpdateProfile.as_view(),name='update_profile'),
 	path('register/',views.Register.as_view(),name='register'),
 	path('',include('django.contrib.auth.urls')),
